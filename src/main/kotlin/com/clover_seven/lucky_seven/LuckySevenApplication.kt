9ab5c77e01dff76f9a36714beafda80bb6b76b5c
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-09-23 23:48:37 +0900

    Feat: Init Settings

---
+++ src/main/kotlin/com/clover_seven/lucky_seven/LuckySevenApplication.kt
@@ -0,0 +1,11 @@
+package com.clover_seven.lucky_seven
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LuckySevenApplication
+
+fun main(args: Array<String>) {
+	runApplication<LuckySevenApplication>(*args)
+}
---
+++ src/test/kotlin/com/clover_seven/lucky_seven/LuckySevenApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clover_seven.lucky_seven
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LuckySevenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

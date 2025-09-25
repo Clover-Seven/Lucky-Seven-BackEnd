fc982634681e37a7c49b65e9aa5751552cd9a0a8
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-09-25 16:50:07 +0900

    Refactor: Entity & Repository 네이밍 수정

--- src/main/kotlin/com/clover_seven/lucky_seven/db/tarot/repository/TarotResultDetailRepository.kt
+++ src/main/kotlin/com/clover_seven/lucky_seven/db/tarot/repository/TarotResultDetailRepository.kt
@@ -1,9 +1,9 @@
 package com.clover_seven.lucky_seven.db.tarot.repository
 
-import com.clover_seven.lucky_seven.db.tarot.entity.TarotDetailResult
+import com.clover_seven.lucky_seven.db.tarot.entity.TarotResultDetail
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TarotDetailResultRepository : JpaRepository<TarotDetailResult, Long> {
+interface TarotResultDetailRepository : JpaRepository<TarotResultDetail, Long> {
 }

05666fce7084bf42b861571d2112bef904b1793b
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-09-25 16:09:49 +0900

    Fix: UserActiveLogRepository 내 Entity 이름 수정

--- src/main/kotlin/com/clover_seven/lucky_seven/db/user/repository/UserActiveLogRepository.kt
+++ src/main/kotlin/com/clover_seven/lucky_seven/db/user/repository/UserActiveLogRepository.kt
@@ -1,8 +1,9 @@
 package com.clover_seven.lucky_seven.db.user.repository
 
+import com.clover_seven.lucky_seven.db.user.entity.UserActiveLog
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserActiveLogRepository : JpaRepository<UserActiveLogRepository, Long> {
+interface UserActiveLogRepository : JpaRepository<UserActiveLog, Long> {
 }
